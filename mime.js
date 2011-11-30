e4b6e728536b0d87332bcb36ff49d79cf70632c9
fkmr <rkworks@o0o.co>
2011-12-01 02:35:23 +0900

    Decode mime word with non UTF-8

--- mime.js
+++ mime.js
@@ -121,7 +121,7 @@ this.decodeMimeWord = function(str){
   }
 
   if(encoding.toUpperCase()=="B"){
-      return this.decodeBase64(text);
+      return this.decodeBase64(text,charset);
   }
 
   return text;
