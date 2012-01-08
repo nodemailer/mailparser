b43afaee573bdd1ce8cde13d7feed735b9ee94a1
Andris Reinman <andris@node.ee>
2012-01-08 11:39:27 +0200

    fixed the bug when chunks were split in the middle of line ending sequence \r\n

--- test/simple.js
+++ test/simple.js
@@ -1,11 +1,14 @@
 var MailParser = require("../lib/mailparser").MailParser,
-    mailparser = new MailParser();
+    mailparser = new MailParser({
+        debug: true
+    });
 
 var email = "From: 'Sender Name' <sender@example.com>\r\n"+
             "To: 'Receiver Name' <receiver@example.com>\r\n"+
             "Subject: Hello world!\r\n"+
             "\r\n"+
-            "How are you today?";
+            "How are you today?\r\n"+
+            "Another line of text";
 
 // setup an event listener when the parsing finishes
 mailparser.on("end", function(mail_object){
@@ -14,6 +17,8 @@ mailparser.on("end", function(mail_object){
     console.log("Text body:", mail_object.text); // How are you today
 });
 
-// send the email source to the parser
-mailparser.write(email);
+// send the email source to the parser, char by char
+for(var i=0, len=email.length; i<len; i++){
+    mailparser.write(email.charAt(i));
+}
 mailparser.end();
