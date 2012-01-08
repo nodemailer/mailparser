74629ecac94651a0022b9010d0efc302a27b6894
Andris Reinman <andris@node.ee>
2012-01-08 22:12:50 +0200

    smarter html charset detect

--- test/simple.js
+++ test/simple.js
@@ -6,15 +6,17 @@ var MailParser = require("../lib/mailparser").MailParser,
 var email = "From: 'Sender Name' <sender@example.com>\r\n"+
             "To: 'Receiver Name' <receiver@example.com>\r\n"+
             "Subject: Hello world!\r\n"+
+            "Content-type: text/html\r\n"+
             "\r\n"+
-            "How are you today?\r\n"+
+            "<meta charset=\"utf-8\"/>\r\n"+
+            "How are you todayä?\r\n"+
             "Another line of text";
 
 // setup an event listener when the parsing finishes
 mailparser.on("end", function(mail_object){
     console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
     console.log("Subject:", mail_object.subject); // Hello world!
-    console.log("Text body:", mail_object.text); // How are you today
+    console.log("HTML body:", mail_object.html); // How are you today
 });
 
 // send the email source to the parser, char by char
