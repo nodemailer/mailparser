8f137822f99633f46f6cfa8892ace23abc85030a
Andris Reinman <andris@kreata.ee>
2017-02-03 14:39:25 +0200

    v2.0.1. Fixed a typo in code, SimpleParser->simpleParser

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const MailParser = require('./lib/mail-parser');
-const SimpleParser = require('./lib/simple-parser');
+const simpleParser = require('./lib/simple-parser');
 
 module.exports = {
     MailParser,
-    SimpleParser
+    simpleParser
 };
