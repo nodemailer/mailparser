df68a86061ff05f59e159e6d69be64c6baeb0c0c
Andris Reinman <andris@kreata.ee>
2018-07-24 09:07:00 +0300

    debug

--- test/mail-parser-test.js
+++ test/mail-parser-test.js
@@ -360,6 +360,7 @@ exports['Binary attachment encodings'] = {
         });
     },
     Base64: test => {
+        console.log('========');
         let encodedText = 'Content-Type: application/octet-stream\r\nContent-Transfer-Encoding: base64\r\n\r\nAAECA/3+/w==',
             mail = Buffer.from(encodedText, 'utf-8');
 
@@ -367,18 +368,22 @@ exports['Binary attachment encodings'] = {
         let mailparser = new MailParser();
         mailparser.end(mail);
         mailparser.on('data', data => {
+            console.log(data.type);
             if (data.type === 'attachment') {
                 let chunks = [];
                 data.content.on('data', chunk => chunks.push(chunk));
                 data.content.on('end', () => {
                     data.content = Buffer.concat(chunks);
+                    console.log('attachment end');
                     data.release();
                 });
                 attachments.push(data);
             }
         });
         mailparser.on('end', () => {
+            console.log('parser end');
             test.equal(Array.prototype.slice.apply((attachments[0].content && attachments[0].content) || []).join(','), '0,1,2,3,253,254,255');
+            console.log('========');
             test.done();
         });
     },
@@ -561,7 +566,7 @@ exports['Attachment filename'] = {
                 'Content-Type: application/octet-stream\r\n' +
                 'Content-Transfer-Encoding: QUOTED-PRINTABLE\r\n' +
                 'Content-Disposition: attachment;\r\n' +
-                '    filename*0*=UTF-8\'\'%C3%95%C3%84;\r\n' +
+                '    filename*0*=UTF-8\x27\x27%C3%95%C3%84;\r\n' +
                 '    filename*1*=%C3%96%C3%9C\r\n' +
                 '\r\n' +
                 '=00=01=02=03=FD=FE=FF',
@@ -591,7 +596,7 @@ exports['Attachment filename'] = {
                 'Content-Type: application/octet-stream\r\n' +
                 'Content-Transfer-Encoding: QUOTED-PRINTABLE\r\n' +
                 'Content-Disposition: attachment;\r\n' +
-                '    filename*0*=UTF-8\'\'%C3%95%C3%84;\r\n' +
+                '    filename*0*=UTF-8\x27\x27%C3%95%C3%84;\r\n' +
                 '    filename*1*=%C3%96%C3%9C;\r\n' +
                 '    filename*2=.txt\r\n' +
                 '\r\n' +
@@ -671,7 +676,7 @@ exports['Attachment filename'] = {
     'Content-Type name*': test => {
         let encodedText =
                 'Content-Type: application/octet-stream;\r\n' +
-                '    name*=UTF-8\'\'%C3%95%C3%84%C3%96%C3%9C\r\n' +
+                '    name*=UTF-8\x27\x27%C3%95%C3%84%C3%96%C3%9C\r\n' +
                 'Content-Transfer-Encoding: QUOTED-PRINTABLE\r\n' +
                 '\r\n' +
                 '=00=01=02=03=FD=FE=FF',
@@ -699,7 +704,7 @@ exports['Attachment filename'] = {
     'Content-Type name*X*': test => {
         let encodedText =
                 'Content-Type: application/octet-stream;\r\n' +
-                '    name*0*=UTF-8\'\'%C3%95%C3%84;\r\n' +
+                '    name*0*=UTF-8\x27\x27%C3%95%C3%84;\r\n' +
                 '    name*1*=%C3%96%C3%9C\r\n' +
                 'Content-Transfer-Encoding: QUOTED-PRINTABLE\r\n' +
                 '\r\n' +
