141c7c5c26fb7bfca5fddd135a2126216701a258
Andris Reinman <andris@kreata.ee>
2018-10-22 19:59:07 +0300

    failing test

--- test/mail-parser-test.js
+++ test/mail-parser-test.js
@@ -5,6 +5,28 @@ const iconv = require('iconv-lite');
 const fs = require('fs');
 
 exports['General tests'] = {
+    'Simple with part as rfc822 of headers only': test => {
+        if (Date.now() < 1) {
+            return test.done();
+        }
+        let encodedText = `Content-Type: multipart/mixed; boundary="ABC"
+
+--ABC
+Content-Type: message/rfc822
+
+Content-Type: text/plain; charset=utf-8
+Subject: OK
+--ABC--`,
+            mail = Buffer.from(encodedText, 'utf-8');
+
+        let mailparser = new MailParser();
+        mailparser.end(mail);
+        mailparser.on('data', () => false);
+        mailparser.on('end', () => {
+            test.equal(mailparser.text, '\nSubject: OK\n\nok');
+            test.done();
+        });
+    },
     'Many chunks': test => {
         let encodedText = 'Content-Type: text/plain; charset=utf-8\r\n\r\nÕÄ\r\nÖÜ', // \r\nÕÄÖÜ
             mail = Buffer.from(encodedText, 'utf-8');
