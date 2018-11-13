7db9ed490b9d38c7f24339af33af390aef5f16d2
Andris Reinman <andris@kreata.ee>
2018-11-13 11:25:28 +0200

    v2.4.2

---
+++ examples/simple-options.js
@@ -0,0 +1,22 @@
+/* eslint no-console:0 */
+
+'use strict';
+
+const util = require('util');
+const fs = require('fs');
+const simpleParser = require('../lib/simple-parser.js');
+
+async function main() {
+    let mail = await simpleParser(fs.createReadStream(__dirname + '/nodemailer.eml'), {
+        skipImageLinks: true, // do not convert CID attachments to data URL images
+        skipTextToHtml: false, // generate HTML form plaintext if needed
+        skipTextLinks: true, // do not linkify links in plaintext content
+        formatDateString: date => date.toUTCString() // format date in RFC822 embedded HTML head section
+    });
+
+    console.log(util.inspect(mail, false, 22));
+}
+
+main().catch(err => {
+    console.log(err);
+});
