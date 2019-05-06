9463fe8fc31b3436ea3941a396303ff8adc877d6
Mathias Lundell <lundell.mathias@gmail.com>
2019-05-06 17:27:16 +0200

    Add option to skip html to text conversion

--- examples/simple-options.js
+++ examples/simple-options.js
@@ -9,7 +9,8 @@ const simpleParser = require('../lib/simple-parser.js');
 async function main() {
     let mail = await simpleParser(fs.createReadStream(__dirname + '/nodemailer.eml'), {
         skipImageLinks: true, // do not convert CID attachments to data URL images
-        skipTextToHtml: false, // generate HTML form plaintext if needed
+        skipHtmlToText: false, // generate plaintext from HTML if needed
+        skipTextToHtml: false, // generate HTML from plaintext if needed
         skipTextLinks: true, // do not linkify links in plaintext content
         formatDateString: date => date.toUTCString() // format date in RFC822 embedded HTML head section
     });
