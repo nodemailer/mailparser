a6457606ef608a7b4c2f2597cfb4fa0aaa88e134
Andris Reinman <andris@reinman.eu>
2023-03-03 11:22:07 +0200

    Do not repeat processing invalidly encoded address

--- test/issue-337-test.js
+++ test/issue-337-test.js
@@ -3,7 +3,6 @@
 const simpleParser = require('..').simpleParser;
 
 module.exports['Hangs when Reply-To invalid encoded'] = async test => {
-    simpleParser('Content-type: text/plain\r\nReply-To: =?U� -8?B?0=D0=BA=D0=BE=D0=B2?=\n\r\n1234');
-
+    await simpleParser('Content-type: text/plain\r\nReply-To: =?U� -8?B?0=D0=BA=D0=BE=D0=B2?=\n\r\n1234');
     test.done();
 };
